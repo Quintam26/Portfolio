1a4fda46e8766bcd590cf4636ad65f9941815eae
Mario Quintana <pyrros2000@gmail.com>
2018-04-29 15:56:27 -0700

    added project data and constructor function. Forgot to switch branch....

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,10 @@
-// constructor function
+'use strict';
+
+function projects (projectsDataObj) {
+    // TODO: Use the JS object passed in to complete this constructor function:
+    // Save ALL the properties of `rawDataObj` into `this`
+     this.title = projectsDataObj.title;
+     this.image= projectsDataObj.image;
+     this.body = projectsDataObj.body;
+  }
+  
