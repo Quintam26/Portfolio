281bb2bc87adc72bedf92a8a062ef01acf4758c5
Mario Quintana <pyrros2000@gmail.com>
2018-06-05 18:46:50 -0700

    restored portfolio data to render due to calling a function twice

--- scripts/controller/homeController.js
+++ scripts/controller/homeController.js
@@ -4,7 +4,9 @@
 const homeController = {};
 
 homeController.show = function() {
-    $('#page1, #page2, #page3, #page4').show();   
+    $('#page1, #page2, #page3, #page4').show();
+    
+   
 }
 
 module.homeController = homeController;
--- scripts/controller/projectController.js
+++ scripts/controller/projectController.js
@@ -5,7 +5,6 @@ const projectController = {};
     
 projectController.show = function() {
     $('#page1, #page2, #page4').hide();
-    Project.fetchAll();
     $('#page3').show();    
 }
     
